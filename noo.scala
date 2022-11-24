bd3e0336ba736d198c1ef479bb515db87ed48c03
Jisoo Park <xxxyel@gmail.com>
2022-11-24 14:18:16 +0900

    Dedicated datatype

--- noo.scala
+++ noo.scala
@@ -9,27 +9,30 @@ import zio.http.model.Method
 
 object Main extends ZIOAppDefault {
 
+  case class Property(name: String, description: String)
+
+  val properties = Vector(
+    Property("1. Levels of Scale", ""),
+    Property("2. Strong Centers", ""),
+    Property("3. Boundaries", ""),
+    Property("4. Alternating Repetition", ""),
+    Property("5. Positive Space", ""),
+    Property("6. Good Shape", ""),
+    Property("7. Local Symmetries", ""),
+    Property("8. Deep Interlock and Ambiguity", ""),
+    Property("9. Contrast", ""),
+    Property("10. Gradients", ""),
+    Property("11. Roughness", ""),
+    Property("12. Echoes", ""),
+    Property("13. The Void", ""),
+    Property("14. Simplicity and Inner Calm", ""),
+    Property("15. Not-separatedness", ""),
+  )
+
   val app: HttpApp[Any, Nothing] = Http.collect[Request] {
     case Method.GET -> !! / "random-property" =>
-      val properties = Vector(
-        "1. Levels of Scale",
-        "2. Strong Centers",
-        "3. Boundaries",
-        "4. Alternating Repetition",
-        "5. Positive Space",
-        "6. Good Shape",
-        "7. Local Symmetries",
-        "8. Deep Interlock and Ambiguity",
-        "9. Contrast",
-        "10. Gradients",
-        "11. Roughness",
-        "12. Echoes",
-        "13. The Void",
-        "14. Simplicity and Inner Calm",
-        "15. Not-separatedness",
-      )
       val picked = scala.util.Random.shuffle(properties).head
-      Response.text(picked)
+      Response.text(picked.name)
   }
 
   override val run =
