e8e96354948255643a0708b48ca7ff1f72bea457
Jisoo Park <xxxyel@gmail.com>
2022-11-24 14:11:10 +0900

    Add zio-http

--- noo.scala
+++ noo.scala
@@ -1,10 +1,18 @@
 //> using scala "2.13.10"
-//> using option "-source:3"
+//> using option "-Xsource:3"
 //> using lib "dev.zio::zio:2.0.4"
+//> using lib "dev.zio::zio-http:0.0.3"
 
 import zio.*
+import zio.http.*
+import zio.http.model.Method
 
 object Main extends ZIOAppDefault {
-  val run =
-    Console.printLine("Strong Centers")
+
+  val app: HttpApp[Any, Nothing] = Http.collect[Request] {
+    case Method.GET -> !! / "random-property" => Response.text("Strong Centers")
+  }
+
+  override val run =
+    Server.serve(app).provide(Server.default)
 }
